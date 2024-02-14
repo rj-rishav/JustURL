8ade99e8d82383f10bde4c0c15f40af979fb832e
rj-rishav <chatwithme146@gmail.com>
2024-02-14 19:52:32 +0530

    Added views folder and files in it

--- app.js
+++ app.js
@@ -2,15 +2,24 @@ require("dotenv").config();
 const express = require("express");
 const connectDB = require("./connection");
 const router = require("./routes/urlRoute");
+const axios = require("axios");
 
 const errorMidleware = require("./middlewares/errorMiddleware");
 
 connectDB();
 const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static("node_modules/bootstrap/dist"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use("/", router);
+app.use("/api", router);
+app.get("/", async (req, res) => {
+  const response = await axios.get("http://localhost:5005/api/urls");
+  const data = response.data;
+  res.render("home", { data });
+});
 
 app.use(errorMidleware);
 
